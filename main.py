cdf36fd37038066a83c40d89fc93038a8f90e415
Maaattqc <mathieufournierqc@outlook.com>
2022-09-06 11:33:46 -0400

    wow insane new file working

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ import RPi.GPIO as GPIO
 import dht11
 
 # initialize GPIO
+from paho import mqtt
+import paho.mqtt.client as mqtt
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 instance = dht11.DHT11(pin = 27)
+client = mqtt.Client()
+client.connect("205.236.77.2", 1883, 60)
 
 # read data using pin 14
 while True:
+    client.loop_forever()
     try:
         time.sleep(1.0)
 
         result = instance.read()
-
         if result.is_valid():
             print("Temperature: %-3.1f C" % result.temperature)
             print("Humidity: %-3.1f %%" % result.humidity)
         else:
             print("Error: %d" % result.error_code)
+
     except:
         print("Error: %d" % result.error_code)
