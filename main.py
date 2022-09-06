b970ce5545b0d0f9402a6397506c5e11a94fc6e9
Maaattqc <mathieufournierqc@outlook.com>
2022-09-06 10:09:19 -0400

    wow insane new file

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import RPi.GPIO as GPIO
+import dht11
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# initialize GPIO
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
 
+# read data using pin 14
+instance = dht11.DHT11(pin = 14)
+result = instance.read()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if result.is_valid():
+    print("Temperature: %-3.1f C" % result.temperature)
+    print("Humidity: %-3.1f %%" % result.humidity)
+else:
+    print("Error: %d" % result.error_code)
