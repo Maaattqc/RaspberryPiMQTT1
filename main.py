3b42cef643eef06417735cb08b4c9f07fbc07357
Maaattqc <mathieufournierqc@outlook.com>
2022-09-07 13:54:20 -0400

    wow insane new file working

--- main.py
+++ main.py
@@ -11,21 +11,37 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 instance = dht11.DHT11(pin = 27)
-client = mqtt.Client()
-client.connect("205.236.77.2", 1883, 60)
-
 # read data using pin 14
+
+broker_url = "10.4.1.212"
+broker_port = 1883
+
+client = mqtt.Client()
+client.connect(broker_url, broker_port)
+
+
+def on_connect(client, userdata, flags, rc):
+   print("Connected With Result Code: {}".format(rc))
+
+def on_disconnect(client, userdata, rc):
+   print("Client Got Disconnected")
+
 while True:
-    client.loop_forever()
+
     try:
+
         time.sleep(1.0)
 
         result = instance.read()
+
+        client.loop()
         if result.is_valid():
             print("Temperature: %-3.1f C" % result.temperature)
             print("Humidity: %-3.1f %%" % result.humidity)
+            client.publish(topic="TestingTopic", payload="Temperature: %-3.1f C" % result.temperature, qos=1, retain=False)
         else:
             print("Error: %d" % result.error_code)
+
 
     except:
         print("Error: %d" % result.error_code)
